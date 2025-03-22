59eb6d21b4ec309cbb01bebf47efb25fd99c4a79
marlisonmourao <bentesmourao@gmail.com>
2025-03-22 01:37:15 -0400

    feat: guard auth in app

---
+++ purchases/src/http/auth/authorization/authorization.guard.ts
@@ -0,0 +1,38 @@
+import {
+  type CanActivate,
+  type ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common'
+import type { ConfigService } from '@nestjs/config'
+import { auth } from 'express-oauth2-jwt-bearer'
+
+@Injectable()
+export class AuthorizationGuard implements CanActivate {
+  private AUTH0_AUDIENCE: string
+  private AUTH0_DOMAIN: string
+
+  constructor(private configService: ConfigService) {
+    this.AUTH0_AUDIENCE = this.configService.get('AUTH0_AUDIENCE') ?? ''
+    this.AUTH0_DOMAIN = this.configService.get('AUTH0_DOMAIN') ?? ''
+  }
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest()
+    const response = context.switchToHttp().getResponse()
+
+    const checkJWT = auth({
+      audience: this.AUTH0_AUDIENCE,
+      issuerBaseURL: this.AUTH0_DOMAIN,
+      tokenSigningAlg: 'RS256',
+    })
+
+    try {
+      checkJWT(request, response, () => {})
+
+      return true
+    } catch (error) {
+      throw new UnauthorizedException(error)
+    }
+  }
+}
