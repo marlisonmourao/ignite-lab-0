fc5d98f6ef072691308b468bd0be33dfd77ec375
marlisonmourao <bentesmourao@gmail.com>
2025-03-24 19:10:24 -0400

    feat: implement Kafka microservice for purchase events and add MessagingModule

--- classroom/src/main.ts
+++ classroom/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core'
+import { type MicroserviceOptions, Transport } from '@nestjs/microservices'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3334)
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        clientId: 'classroom',
+        brokers: ['localhost:29092'],
+      },
+    },
+  })
+
+  app
+    .startAllMicroservices()
+    .then(() => console.log('Microservices are listening'))
+
+  app.listen(3334).then(() => console.log('HTTP Server is listening'))
 }
 bootstrap()
